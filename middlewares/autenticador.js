7e1c3c3c97ea383b96d20ea543d234186d5c0a5e
Miguel Soeth <miguelsoeth@gmail.com>
2024-04-27 19:14:11 -0300

    Acesso por usuario

--- middlewares/autenticador.js
+++ middlewares/autenticador.js
@@ -5,10 +5,10 @@ const crypto = require('crypto');
 const users = JSON.parse(fs.readFileSync('./data/users.json'));
 
 function autenticador(req, res, next) {
-  const { email, password } = req.body;
+  const { account, password } = req.body;
   const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
 
-  const user = users.find(u => u.email === email && u.pwd === hashedPassword);
+  const user = users.find(u => (u.email === account || u.user === account) && u.pwd === hashedPassword);
 
   if (user) {
     req.session.user = user;
